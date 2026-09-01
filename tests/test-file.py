e015f56ac978e8642550f21c83c5d6aecc8895ba
meerabtahirkhan214-collab <meerabtahirkhan214@gmail.com>
2026-09-01 19:44:18 +0500

    Update test-file.py
    Add comments to test file

--- tests/test-file.py
+++ tests/test-file.py
@@ -1,3 +1,5 @@
+# sample secrets used to test the scanners regex patterns
+# All values below are fake and safe-used only for detection testing
 aws_key = "AKIAABCDEFGHIJKLMNOP"
 google_key = "AIzaSyD1234567890abcdefghijklmnopqr"
 github_token = "ghp_1234567890abcdefghijklmnopqrstuvwx"
